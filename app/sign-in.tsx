32f627b5fdaf23ad4637ad0d12d84f001fb0e9cd
Ilyas0236 <ilyasaitmaina@gmail.com>
2026-01-04 23:09:31 +0100

    MODUIF

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,4 +1,4 @@
-import {  ScrollView, Image } from "react-native";
+import {ScrollView, Image, View, Text} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from 'react';
 
@@ -6,12 +6,24 @@ import images from "@/constants/images";
 const SignIn = () => {
     return (
         <SafeAreaView className="bg-white h-full">
-            <ScrollView className="h-full">
+            <ScrollView contentContainerClassName="h-full">
                 <Image
                     source={images.onboarding}
                     className="w-full h-4/6"
                     resizeMode="contain"
                 />
+                <View className="px-10">
+                    <Text className="text-base text-center uppercase font-rubik text-black-200">Welcome to Restate</Text>
+
+                    <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2" >
+                        Let's Get You Closer to {"\n"}
+                        <Text className="text-primary-300"> Your Ideal Home</Text>
+                    </Text>
+
+
+
+                </View>
+
             </ScrollView>
         </SafeAreaView>
     );
