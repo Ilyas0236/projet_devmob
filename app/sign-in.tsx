1d78d350bcc3f2dc0b1829369f3b4a4d12ba736c
Ilyas0236 <ilyasaitmaina@gmail.com>
2026-01-04 23:32:47 +0100

    MODUIF

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,9 +1,12 @@
-import {ScrollView, Image, View, Text} from "react-native";
+import {ScrollView, Image, View, Text, Button, Touchable, TouchableOpacity} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from 'react';
 
 import images from "@/constants/images";
+import icons from "@/constants/icons";
 const SignIn = () => {
+const handleLogin =() => {};
+
     return (
         <SafeAreaView className="bg-white h-full">
             <ScrollView contentContainerClassName="h-full">
@@ -19,6 +22,29 @@ const SignIn = () => {
                         Let's Get You Closer to {"\n"}
                         <Text className="text-primary-300"> Your Ideal Home</Text>
                     </Text>
+
+                    <Text className="text-lg font-rubik text-black-200 text-center mt-12" >
+                        Login to Restate with Google
+                    </Text>
+
+                    <TouchableOpacity onPress={handleLogin} className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5">
+
+                        <View className="flex flex-row items-center justify-center" >
+                            <Image
+                                source={icons.google}
+                                className="w-5 h-5"
+                                resizeMode="contain"
+                            />
+                            <Text className="text-lg font-rubik-medium text-black-300 ml-2">Continue with Google</Text>
+
+                        </View>
+
+
+
+
+                    </TouchableOpacity>
+
+
 
 
 
